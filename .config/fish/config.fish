c3668ff25eced3d5961fda2ca0cd7d9eb7a69799
rusa gedougawa <rusa.gedougawa@gmail.com>
2019-11-11 02:21:24 +0900

    fix omf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -x LESSCHARSET utf-8
 set -x LC_CTYPE ja_JP.UTF-8
 set -x GOPATH $HOME/dev
 set -x PATH $PATH $GOPATH/bin
+set -g theme_powerline_fonts no
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
