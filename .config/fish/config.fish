2785ef1e2cae32cd2e6f9a8ebaa47abe4c9325cd
rusa gedougawa <rusa.gedougawa@gmail.com>
2019-11-11 02:12:40 +0900

    add files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
+set -x LANG ja_JP.UTF-8
+set -x LESSCHARSET utf-8
+set -x LC_CTYPE ja_JP.UTF-8
 set -x GOPATH $HOME/dev
 set -x PATH $PATH $GOPATH/bin
 
-alias ls='ls --color'
-alias grep='grep --color'
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
